e2010c31f6e2b3719191758c356d5bbc5f93649e
pablouix <pablouix@gmail.com>
2022-09-23 19:16:59 -0400

    Agregando Database

---
+++ app/src/main/java/edu/ucne/parcial1_pablo/data/AppDataBase.kt
@@ -0,0 +1,14 @@
+package edu.ucne.parcial1_pablo.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.parcial1_pablo.data.entity.EntidadDeMentira
+
+@Database(
+    entities = [EntidadDeMentira::class],
+    version = 1
+)
+
+abstract class AppDataBase : RoomDatabase(){
+
+}
