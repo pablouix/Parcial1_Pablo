9ada35e9f892c861d9246a4350b67053c79b3068
pablouix <pablouix@gmail.com>
2022-09-28 18:37:36 -0400

    Quitando comentario a list view model

--- app/src/main/java/edu/ucne/parcial1_pablo/ui/articulos_list/ArticulosListViewModel.kt
+++ app/src/main/java/edu/ucne/parcial1_pablo/ui/articulos_list/ArticulosListViewModel.kt
@@ -1,11 +1,15 @@
 package edu.ucne.parcial1_pablo.ui.articulos_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.parcial1_pablo.Repositorio.ArticulosRepositorio
 import edu.ucne.parcial1_pablo.data.entity.Articulo
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 data class EntidadMentiraUiState(
@@ -16,17 +20,16 @@ data class EntidadMentiraUiState(
 
 @HiltViewModel
 class EntidadMentiraListViewModel @Inject constructor(
-    //val repository :
+    val repositorio: ArticulosRepositorio
 ) : ViewModel(){
 
     private val _uiState = MutableStateFlow(EntidadMentiraUiState())
     val uiState: StateFlow<EntidadMentiraUiState> = _uiState.asStateFlow()
 
-    /*
 
     init {
         viewModelScope.launch {
-            repository.getAll().collect {
+            repositorio.getAll().collect {
                 list ->
                 _uiState.update {
                     it.copy(entidadmentira = list)
@@ -34,6 +37,6 @@ class EntidadMentiraListViewModel @Inject constructor(
             }
         }
     }
-    */
+
 }
 
